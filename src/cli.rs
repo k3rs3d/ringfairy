a0e54660419ca6f976dc7065ac946c9e70633f65
Kern <mccoolname@gmail.com>
2024-02-02 16:42:13 -0800

    Fix error when no config file specified

--- src/cli.rs
+++ src/cli.rs
@@ -1,4 +1,4 @@
-use clap::{Parser, ArgAction};
+use clap::{ArgAction, Parser};
 use serde::Deserialize;
 
 use crate::file;
@@ -32,39 +32,7 @@ impl Default for AppSettings {
     }
 }
 
-// Clap-specific settings struct - able to contain Options
-#[derive(Parser, Debug)]
-#[clap(name = "rustring", version = "0.1.0", author = "Kern AKA Kersed", about = "Generates HTML files to create a static webring system.")]
-pub struct ClapSettings {
-    #[clap(short = 'c', long = "cfg", default_value("./config.json"), ignore_case = false, help = "Specify the config file path")]
-    pub filepath_config: String,
-
-    #[clap(short = 'l', long = "list", ignore_case = false, help = "Specify the file containing the list of websites to use as input")]
-    pub filepath_list: Option<String>,
-
-    #[clap(short = 'o', long = "path-output", ignore_case = false, help = "Define the directory where the generated files will be saved.")]
-    pub path_output: Option<String>,
-
-    #[clap(short = 'a', long = "path-assets", ignore_case = false, help = "Specify the directory where asset files (e.g. CSS, images, other extras) can be found. NOTE: All contents will be copied into the output directory!")]
-    pub path_assets: Option<String>,
-
-    #[clap(short = 'r', long = "path-template-redirect", ignore_case = false, help = "Specify the HTML template used to generate each website's redirect pages. It should contain '<!-- REDIRECT -->' somewhere in the file.")]
-    pub filepath_template_redirect: Option<String>,
-
-    #[clap(short = 'i', long = "path-template-index", ignore_case = false, help = "Specify the HTML template used to generate the main list page. It should contain '<!-- TABLE_OF_WEBSITES -->' somewhere in the file.")]
-    pub filepath_template_index: Option<String>,
-    
-    #[clap(short = 'v', long, action = ArgAction::SetTrue, help = "Enables verbose logging")]
-    pub verbose: bool,
-    
-    #[clap(long = "skip-verification", action = ArgAction::SetTrue, help = "Skips verification of the URLs in the list. Probably unwise!")]
-    pub skip_verify: bool,
-    
-    #[clap(long = "dry-run", action = ArgAction::SetTrue, help = "Perform a dry run without writing any files")]
-    pub dry_run: bool,
-}
-
-// Contains settings loaded from config file, e.g., config.json 
+// Contains settings loaded from config file, e.g., config.json
 #[derive(Deserialize, Debug)]
 pub struct ConfigSettings {
     pub filepath_list: Option<String>,
@@ -77,12 +45,95 @@ pub struct ConfigSettings {
     pub dry_run: Option<bool>,
 }
 
+// Clap-specific settings struct - able to contain Options
+#[derive(Parser, Debug)]
+#[clap(
+    name = "rustring",
+    version = "0.1.0",
+    author = "Kern AKA Kersed",
+    about = "Generates HTML files to create a static webring system."
+)]
+pub struct ClapSettings {
+    #[clap(
+        short = 'c',
+        long = "cfg",
+        ignore_case = false,
+        help = "Specify the config file path. Useful for settings that stay constant across many runs of your application, like path locations. Remember, any settings specified via command-line arguments will override the corresponding ones from this file"
+    )]
+    pub filepath_config: Option<String>,
+
+    #[clap(
+        short = 'l',
+        long = "list",
+        ignore_case = false,
+        help = "Specify the file containing the list of websites to use as input"
+    )]
+    pub filepath_list: Option<String>,
+
+    #[clap(
+        short = 'o',
+        long = "path-output",
+        ignore_case = false,
+        help = "Define the directory where the generated files will be saved."
+    )]
+    pub path_output: Option<String>,
+
+    #[clap(
+        short = 'a',
+        long = "path-assets",
+        ignore_case = false,
+        help = "Specify the directory where asset files (e.g. CSS, images, other extras) can be found. NOTE: All contents will be copied into the output directory!"
+    )]
+    pub path_assets: Option<String>,
+
+    #[clap(
+        short = 'r',
+        long = "path-template-redirect",
+        ignore_case = false,
+        help = "Specify the HTML template used to generate each website's redirect pages. It should contain '<!-- REDIRECT -->' somewhere in the file."
+    )]
+    pub filepath_template_redirect: Option<String>,
+
+    #[clap(
+        short = 'i',
+        long = "path-template-index",
+        ignore_case = false,
+        help = "Specify the HTML template used to generate the main list page. It should contain '<!-- TABLE_OF_WEBSITES -->' somewhere in the file."
+    )]
+    pub filepath_template_index: Option<String>,
+
+    #[clap(short = 'v', long, action = ArgAction::SetTrue, help = "Enables verbose logging")]
+    pub verbose: bool,
+
+    #[clap(long = "skip-verification", action = ArgAction::SetTrue, help = "Skips verification of the URLs in the list. Probably unwise!")]
+    pub skip_verify: bool,
+
+    #[clap(long = "dry-run", action = ArgAction::SetTrue, help = "Perform a dry run without writing any files")]
+    pub dry_run: bool,
+}
+
 async fn load_config(config_path: &str) -> Option<ConfigSettings> {
+    // Early return for an empty path
+    if config_path.trim().is_empty() {
+        return None;
+    }
+
+    // Load async (supports either locally or remotely)
     let config_content = match file::acquire_file_data(config_path).await {
         Ok(content) => content,
         Err(_) => return None,
     };
-    serde_json::from_str(&config_content).ok()
+
+    // Ensure the file is not empty
+    if config_content.trim().is_empty() {
+        return None;
+    }
+
+    // Attempt to deserialize and return the result
+    match serde_json::from_str(&config_content) {
+        Ok(config) => Some(config), // Done
+        Err(_) => None,             // Deserialization failed
+    }
 }
 
 fn merge_configs(cli_args: ClapSettings, config: Option<ConfigSettings>) -> AppSettings {
@@ -93,31 +144,56 @@ fn merge_configs(cli_args: ClapSettings, config: Option<ConfigSettings>) -> AppS
         final_settings.filepath_list = conf.filepath_list.unwrap_or(final_settings.filepath_list);
         final_settings.path_output = conf.path_output.unwrap_or(final_settings.path_output);
         final_settings.path_assets = conf.path_assets.unwrap_or(final_settings.path_assets);
-        final_settings.filepath_template_redirect = conf.filepath_template_redirect.unwrap_or(final_settings.filepath_template_redirect);
-        final_settings.filepath_template_index = conf.filepath_template_index.unwrap_or(final_settings.filepath_template_index);
+        final_settings.filepath_template_redirect = conf
+            .filepath_template_redirect
+            .unwrap_or(final_settings.filepath_template_redirect);
+        final_settings.filepath_template_index = conf
+            .filepath_template_index
+            .unwrap_or(final_settings.filepath_template_index);
         final_settings.verbose = conf.verbose.unwrap_or(final_settings.verbose);
         final_settings.skip_verify = conf.skip_verify.unwrap_or(final_settings.skip_verify);
         final_settings.dry_run = conf.dry_run.unwrap_or(final_settings.dry_run);
     }
 
     // Then, override with CLI arguments if provided
-    if let Some(val) = cli_args.filepath_list { final_settings.filepath_list = val; }
-    if let Some(val) = cli_args.path_output { final_settings.path_output = val; }
-    if let Some(val) = cli_args.path_assets { final_settings.path_assets = val; }
-    if let Some(val) = cli_args.filepath_template_redirect { final_settings.filepath_template_redirect = val; }
-    if let Some(val) = cli_args.filepath_template_index { final_settings.filepath_template_index = val; }
+    if let Some(val) = cli_args.filepath_list {
+        final_settings.filepath_list = val;
+    }
+    if let Some(val) = cli_args.path_output {
+        final_settings.path_output = val;
+    }
+    if let Some(val) = cli_args.path_assets {
+        final_settings.path_assets = val;
+    }
+    if let Some(val) = cli_args.filepath_template_redirect {
+        final_settings.filepath_template_redirect = val;
+    }
+    if let Some(val) = cli_args.filepath_template_index {
+        final_settings.filepath_template_index = val;
+    }
 
     // Boolean flags can simply be overridden as they don't have a `None` state
-    if cli_args.verbose { final_settings.verbose = cli_args.verbose; }
-    if cli_args.skip_verify { final_settings.skip_verify = cli_args.skip_verify; }
-    if cli_args.dry_run { final_settings.dry_run = cli_args.dry_run; }
-    
+    if cli_args.verbose {
+        final_settings.verbose = cli_args.verbose;
+    }
+    if cli_args.skip_verify {
+        final_settings.skip_verify = cli_args.skip_verify;
+    }
+    if cli_args.dry_run {
+        final_settings.dry_run = cli_args.dry_run;
+    }
+
     final_settings
 }
 
 pub async fn parse_args() -> AppSettings {
     let clap_args = ClapSettings::parse();
-    let config_args = load_config(&clap_args.filepath_config).await;
+
+    // Check if a config file path is provided, and it's not empty
+    let config_args = match clap_args.filepath_config.as_deref() {
+        Some("") | Some("none") | None => None, // Treat as no config specified
+        Some(path) => load_config(path).await,
+    };
 
     merge_configs(clap_args, config_args)
 }
