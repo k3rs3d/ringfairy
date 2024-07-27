4ad7287ac96766497e503cd99018623dd5890b00
Kern <mccoolname@gmail.com>
2024-07-27 12:40:49 -0700

    Cleanup main.rs

--- src/main.rs
+++ src/main.rs
@@ -8,29 +8,28 @@ mod website;
 
 #[tokio::main]
 async fn main() {
+    // Init logging
+    env_logger::init();
+
+    // Parse arguments & get settings struct
+    let settings = cli::parse_args().await;
+    log::info!("Starting generator with settings: {:?}", settings);
+
     // Start a timer 
     let start = std::time::Instant::now();
 
-    // Parse the arguments and get settings struct
-    let settings = cli::parse_args().await;
-
-    // Init logging
-    env_logger::init();
-    log::info!("Starting generator with settings: {:?}", settings);
-
-    // Perform webring generation
+    // Generate webring 
     if let Err(e) = website::process_websites(&settings).await {
         log::error!("Process error: {}", e);
         return;
     }
 
-    // Finally, copy files from ./assets (by default) into the output folder
+    // Finally, copy static files (from ./assets by default) into output folder
     if let Err(e) = file::copy_asset_files(&settings.path_assets, &settings.path_output) {
         log::error!("Error copying asset file(s): {}", e);
     }
 
-    // Calculate the elapsed time.
+    // Calculate elapsed time
     let elapsed = start.elapsed();
-
     log::info!("Done in {} ms!", elapsed.as_millis());
 }
