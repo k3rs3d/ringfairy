e83fc5f37fd6687a040ed4850f5d0f663180d6ec
Kern <mccoolname@gmail.com>
2024-07-27 11:44:39 -0700

    Add new errors

--- src/error.rs
+++ src/error.rs
@@ -2,15 +2,18 @@
 pub enum Error {
     #[error("Template error: {0}")]
     TemplateError(#[from] tera::Error),
-    
+
     #[error("HTTP error: {0}")]
     HttpError(#[from] reqwest::Error),
-    
+
     #[error("Config error: {0}")]
     ConfigError(#[from] clap::Error),
 
     #[error("I/O error: {0}")]
     IOError(#[from] std::io::Error),
+
+    #[error("Utf8 error: {0}")]
+    Utf8Error(#[from] std::string::FromUtf8Error),
 
     #[error("Error: {0}")]
     StringError(String),
